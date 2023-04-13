7e807de7793c9d9ab9f464f4013543af0cb3a9f4
Maksym Voloshyn <mv.webdew@gmail.com>
2023-04-13 22:45:17 +0300

    feat: add a solutions to the sort-by-height and sum-digits tasks

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let sortedArr = arr.filter((el) => el != -1).sort((a, b) => a - b);
+
+  if (sortedArr.length == arr.length) {
+    return sortedArr;
+  } else {
+    let newArr = arr.map((el) => {
+      if (el !== -1) {
+        return "change";
+      } else {
+        return el;
+      }
+    });
+
+    for (let i = 0; i < sortedArr.length; i += 1) {
+      for (let j = 0; j < newArr.length; j += 1) {
+        if (newArr[j] == "change") {
+          newArr[j] = sortedArr[i];
+
+          break;
+        }
+      }
+    }
+
+    return newArr;
+  }
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
+
+console.log(sortByHeight([11, 16, 2, 2, 4]));
